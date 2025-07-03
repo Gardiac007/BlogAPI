0c136acc2a967e753993dce459e642aee4f420c1
Priyan <71456765+Gardiac007@users.noreply.github.com>
2025-07-03 15:58:57 +0530

    added comments

--- blog_app/router.py
+++ blog_app/router.py
@@ -2,5 +2,5 @@ from rest_framework.routers import DefaultRouter
 from .views import PostView
 
 post_router = DefaultRouter()
-post_router.register(r'post', PostView)
+post_router.register(r'post', PostView) #router for post request
 
